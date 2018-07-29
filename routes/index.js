1e058f6641508044bd188a8ba13a43f9e554a841
Tomy <tomyleereyes@gmail.com>
2018-07-28 22:05:00 -0700

    adding routes directory

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const axios = require('axios')
 router.post('/getName', async (req, res) => {
 	const { username } = req.body
 	const user = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${username}`)
-	console.log(user)
 })
 
 
