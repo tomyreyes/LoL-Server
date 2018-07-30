0632ed1b42660cd7791515e068699242da2c314e
Tomy <tomyleereyes@gmail.com>
2018-07-30 11:07:33 -0700

    end

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,32 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
-router.post('/getName', async (req, res) => {
-	const { username } = req.body
-	const user = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${username}`)
+
+router.post('/user-matches', async (req, res) => {
+	try {
+		const { summonerName } = req.body
+		const summoner = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}/?api_key=${process.env.API_KEY}`)
+		const { accountId } = summoner.data
+		const matchData = await axios.get(`https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}/?api_key=${process.env.API_KEY}`)
+		const { matches } = matchData.data
+		const latestMatches = matches.slice(0, 4)
+		res.send({ summonerData: summoner.data, latestMatches })
+	} catch (err) {
+		console.log(err)
+	}
 })
+
+router.post('/match-details', async (req, res) => {
+	try {
+		const { matchID } = req.body
+		const matchData = await axios.get(`https://na1.api.riotgames.com/lol/match/v3/matches/${matchID}/?api_key=${process.env.API_KEY}`)
+		res.send({ matchDetails: matchData.data })
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+module.exports = router
 
 
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cords')
+const cors = require('cors')
 const PORT = process.env.PORT || 4000
 const app = express()
+const axios = require('axios')
+const routes = require('./routes')
 
 require('dotenv').config()
 
@@ -12,6 +14,18 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use('/', routes)
+
 app.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`)
 })
+
+
+// async function mock() {
+// 	const user = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/tomy143/?api_key=${process.env.API_KEY}`)
+// 	const { accountId } = user.data
+// 	const matchDetails = await axios.get(`https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}/?api_key=${process.env.API_KEY}`)
+// 	const { matches } = matchDetails.data
+// 	console.log(matches)
+// }
+// mock()
